02b1fe378618c48f7e51ca10e8a35ee52285ae8f
零尾 <lwplwf@qq.com>
2018-04-12 12:13:16 +0800

    Add files via upload

---
+++ log/train_iou_visualization.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+lines = 930346
+result = pd.read_csv('log_iou.txt',
+                     skiprows=[x for x in range(lines) if (x%5)],
+                     error_bad_lines=False, names=['Region Avg IOU', 'Class', 'Obj', 'No Obj', 'Avg Recall', 'count'])
+result.head()
+
+result['Region Avg IOU'] = result['Region Avg IOU'].str.split(': ').str.get(1)
+result['Class'] = result['Class'].str.split(': ').str.get(1)
+result['Obj'] = result['Obj'].str.split(': ').str.get(1)
+result['No Obj'] = result['No Obj'].str.split(': ').str.get(1)
+result['Avg Recall'] = result['Avg Recall'].str.split(': ').str.get(1)
+result['count'] = result['count'].str.split(': ').str.get(1)
+result.head()
+result.tail()
+
+#print(result.head())
+# print(result.tail())
+# print(result.dtypes)
+print(result['Region Avg IOU'])
+
+result['Region Avg IOU'] = pd.to_numeric(result['Region Avg IOU'])
+result['Class'] = pd.to_numeric(result['Class'])
+result['Obj'] = pd.to_numeric(result['Obj'])
+result['No Obj'] = pd.to_numeric(result['No Obj'])
+result['Avg Recall'] = pd.to_numeric(result['Avg Recall'])
+result['count'] = pd.to_numeric(result['count'])
+result.dtypes
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(result['Region Avg IOU'].values, label='Region Avg IoU')
+# ax.plot(result['Class'].values,label='Class')
+# ax.plot(result['Obj'].values,label='Obj')
+# ax.plot(result['No Obj'].values,label='No Obj')
+# ax.plot(result['Avg Recall'].values, label='Avg Recall')
+# ax.plot(result['count'].values,label='count')
+ax.legend(loc='best')
+ax.set_title('The Region Avg IoU curves')
+# ax.set_title('The Avg Recall curves')
+ax.set_xlabel('batches')
+fig.savefig('Avg IoU')
+# fig.savefig('Avg Recall')
